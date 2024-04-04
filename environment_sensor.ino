26ef256413fdbe5fe64bf1bd30ba4647dcefac9e
leandro <lhornos@calicebiotech.com>
2024-04-03 23:42:47 -0300

    Cambio sensores a dht22 y capacitivo

--- environment_sensor.ino
+++ environment_sensor.ino
@@ -1,51 +1,52 @@
-// Include the DHT11 library for interfacing with the sensor.
-#include <DHT11.h>
+// SENSOR DE HUMEDAD Y TEMP DHT22
 
-// Create an instance of the DHT11 class.
-// - For Arduino: Connect the sensor to some Digital I/O Pin (here pin 4).
-DHT11 dht11(4);
-const int sensor_pin = A1;	/* Soil moisture sensor O/P pin */
+#include <AM2302-Sensor.h>
+AM2302::AM2302_Sensor am2302{4};
 
+
+const int soil_sensor_pin = A1;	/* Soil moisture sensor O/P pin */
+
+// Calibracion del sensor capacitivo.
+const int air = 805; // Sensor de humedad del suelo al aire
+const int water = 410; // Sensor de humedad del suelo en agua
 
 void setup() {
   // Initialize serial communication to allow debugging and data readout.
     // Using a baud rate of 9600 bps.
-    Serial.begin(9600);
-    
+  Serial.begin(9600);
+  am2302.begin();
+
+  auto status = am2302.read();
     // Uncomment the line below to set a custom delay between sensor readings (in milliseconds).
     // dht11.setDelay(500); // Set this to the desired delay. Default is 500ms.
 }
 
+int count = 0;
+
 void loop() {
-  int temperature = 0;
-  int humidity = 0;
-  float moisture_percentage;
-  int sensor_analog;
+  float soil_moisture_percentage;
+  float raw_soil_moisture;
 
-  // Read moisture
-  sensor_analog = analogRead(sensor_pin);
-  moisture_percentage = ( 100 - ( (sensor_analog/1023.00) * 100 ) );
-
-  // Attempt to read the temperature and humidity values from the DHT11 sensor.
-  int result = dht11.readTemperatureHumidity(temperature, humidity);
-
-  // Check the results of the readings.
-  // If the reading is successful, print the temperature and humidity values.
-  // If there are errors, print the appropriate error messages.
-  if (result == 0) {
-      Serial.print("\nTemperature: ");
-      Serial.print(temperature);
-      Serial.print(" °C\nHumidity: ");
-      Serial.print(humidity);
-  } else {
-      // Print error message based on the error code.
-      Serial.println(DHT11::getErrorString(result));
+  // LEEMOS SENSOR CAPACITIVO DE HUMEDAD DEL SUELO
+  raw_soil_moisture = analogRead(soil_sensor_pin);
+  soil_moisture_percentage = 100 - map(raw_soil_moisture, water, air, 0, 100);
+  if(soil_moisture_percentage > 100){
+    soil_moisture_percentage = 100.0;
   }
-  Serial.print("%\nSoil Moisture Percentage = ");
-  Serial.print(moisture_percentage);
-  Serial.print("%\n");
-  
-  delay(2000);
+  else if (soil_moisture_percentage < 0) {
+    soil_moisture_percentage = 0.0;
+  }
+  auto status = am2302.read();
+  float temperature = am2302.get_Temperature();
+  float humidity = am2302.get_Humidity();
+
+
+  String datosCombinados = String(temperature) + ";" + String(humidity) + ";" + String(soil_moisture_percentage);
+
+  // Ahora puedes usar la variable datosCombinados como necesites
+  Serial.println(datosCombinados);
+  count = count + 1;
+  delay(5000);
 }
 
 
